77553ce7dc9c9868b70589259b640842088637f2
Kevin Samoei <samoeikev@gmail.com>
2019-06-12 12:18:30 +0300

    Add validations to file upload

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,29 @@
 class Document < ApplicationRecord
   has_one_attached :file
+
+  validates_uniqueness_of :title
+  validates_presence_of :title
+  validate :file_presence
+  validate :file_uniqueness
+
+  private
+
+  def file_presence  
+    unless file.attached?
+      errors[:base] << 'No file chosen. A file must be provided'
+      file.purge
+    end
+  end
+
+  def file_uniqueness 
+    documents = Document.all
+    documents.each do |doc|
+      if file.attached?
+        if doc.file.filename == self.file.filename
+          errors[:base] << 'File already uploaded'
+          file.purge
+        end
+      end
+    end
+  end
 end
